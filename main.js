ff6feb52f7fbcbac982a49a775e6a9ae24fdda14
Robert Kerr <robkerr87@gmail.com>
2014-10-23 22:41:17 +1100

    Renamed config.js to auth.js and filtered out tweets without links.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var util = require('util');
 var twitter = require('twitter');
-var config = require('./config.js');
-var twit = new twitter(config);
+var auth = require('./auth.js');
+var twit = new twitter(auth);
 
-twit.search('sam harris', function(data) {
+twit.search('#vegan', function(data) {
   console.log(util.inspect(data['statuses'].map(function(tweet) {
-  	return tweet.text;
+  	// console.log(tweet);
+  	return {user: tweet.user.id, text: tweet.text};
+  }).filter(function(tweet) {
+  	return ((tweet.text.indexOf("http://") > -1) || (tweet.text.indexOf("https://") > -1));
   })));
 });
