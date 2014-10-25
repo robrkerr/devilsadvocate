0e20dfba3659e2595961edfb7dbf3155f3356db4
Robert Kerr <robkerr87@gmail.com>
2014-10-25 12:27:15 +1100

    Promises! Made a function to return the most popular tweets with links based on a particular hashtag.

--- main.js
+++ main.js
@@ -1,13 +1,63 @@
-var util = require('util');
+var RSVP = require('rsvp');
+var promiseRequest = RSVP.denodeify(require('request'));
 var twitter = require('twitter');
 var auth = require('./auth.js');
 var twit = new twitter(auth);
 
-twit.search('#vegan', function(data) {
-  console.log(util.inspect(data['statuses'].map(function(tweet) {
-  	// console.log(tweet);
-  	return {user: tweet.user.id, text: tweet.text};
-  }).filter(function(tweet) {
-  	return ((tweet.text.indexOf("http://") > -1) || (tweet.text.indexOf("https://") > -1));
-  })));
+var twitter_get = function(url) {
+	return new RSVP.Promise(function(resolve, reject) {
+		twit.get(url, function(data) {
+			resolve(data);
+		});
+	});
+};
+
+function expand_url(short_url) {
+	var options = {method: 'HEAD', url: short_url, followAllRedirects: true};
+	return promiseRequest(options).then(function(response) {
+		return response.request.href.split('?')[0];
+	});
+}
+
+function parse_tweet(t) {
+	var expand_promises = t.entities.urls.map(function(url) { 
+		return expand_url(url.expanded_url); 
+	});
+	return RSVP.all(expand_promises).then(function(urls) {
+	  return {
+  		id: t.id_str,
+  		urls: urls,
+  		hashtags: t.entities.hashtags.map(function(tag) { return tag.text; }),
+  		retweet_count: t.retweet_count,
+  		user: t.user.id_str
+  	};
+	});
+}
+
+function parse_and_filter_tweets(tweets) {
+	return RSVP.all(tweets.map(function(t) {
+		return parse_tweet(t);
+	})).then(function(list) {
+		return list.filter(function(t) {
+  		return (t.retweet_count > 0) && (t.urls.length > 0) && (t.hashtags.length > 0);
+  	});
+	});
+}
+
+function get_popular_tweets(hashtag) {
+	var url = '/search/tweets.json?q=%23' + hashtag + '&count=100&result_type=popular';
+	return twitter_get(url).then(function(data) {
+		return parse_and_filter_tweets(data['statuses']);
+	});	
+}
+
+var hashtag = 'islamophobia';
+get_popular_tweets(hashtag).then(function(tweets) {
+	console.log(tweets);
 });
+
+// twit.get('/statuses/retweets/525664776157163520.json', function(data) {
+//   console.log(util.inspect(data.map(function(tweet) {
+//   	return tweet.entities.urls.map(function(url) { return url.expanded_url});
+//   })));
+// });
