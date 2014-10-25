1e7337654dc3a763221ff462c32c8ad6aba6101a
Robert Kerr <robkerr87@gmail.com>
2014-10-25 17:34:44 +1100

    result_type=popular is no good. Changed to get more results than 100.

--- main.js
+++ main.js
@@ -1,34 +1,59 @@
 var RSVP = require('rsvp');
-var promiseRequest = RSVP.denodeify(require('request'));
 var twitter = require('twitter');
+var unshortener = require('unshortener');
 var auth = require('./auth.js');
 var twit = new twitter(auth);
 
 var twitter_get = function(url) {
 	return new RSVP.Promise(function(resolve, reject) {
 		twit.get(url, function(data) {
+			if (data.statusCode) {
+				console.log(data);	
+				reject();
+			}
 			resolve(data);
 		});
 	});
 };
 
 function expand_url(short_url) {
-	var options = {method: 'HEAD', url: short_url, followAllRedirects: true};
-	return promiseRequest(options).then(function(response) {
-		return response.request.href.split('?')[0];
+	return new RSVP.Promise(function(resolve, reject) {
+		unshortener.expand(short_url, function(err, url) {
+			if (err) {
+				console.log(err);
+				resolve(short_url);
+			} else {
+				resolve(url.href.split("?")[0]);
+			}
+		});
 	});
+}
+
+function get_retweeters(tweet) {
+	return new RSVP.Promise(function(resolve, reject) {
+		resolve([]);
+	});
+	// var url = '/statuses/retweets/' + tweet.id_str + '.json';
+	// return twitter_get(url).then(function(data) {
+	// 	return data.map(function(tweet) {
+	// 		return tweet.user.id_str;
+	// 	});
+	// });	
 }
 
 function parse_tweet(t) {
 	var expand_promises = t.entities.urls.map(function(url) { 
 		return expand_url(url.expanded_url); 
 	});
-	return RSVP.all(expand_promises).then(function(urls) {
+	var promises = expand_promises.concat(get_retweeters(t));
+	return RSVP.all(promises).then(function(list) {
+		// console.log(list);
 	  return {
   		id: t.id_str,
-  		urls: urls,
+  		urls: list.slice(0,list.length-1),
   		hashtags: t.entities.hashtags.map(function(tag) { return tag.text; }),
   		retweet_count: t.retweet_count,
+  		retweeters: list[list.length-1].sort(),
   		user: t.user.id_str
   	};
 	});
@@ -44,20 +69,51 @@ function parse_and_filter_tweets(tweets) {
 	});
 }
 
-function get_popular_tweets(hashtag) {
-	var url = '/search/tweets.json?q=%23' + hashtag + '&count=100&result_type=popular';
+function get_tweets(hashtag, count, max_id) {
+	if (!count) {
+		count = 100;
+	}
+	var url = '/search/tweets.json?q=%23' + hashtag;
+	if (count < 100) {
+		url = url + '&count=' + count + '&result_type=recent';
+	} else {
+		url = url + '&count=100&result_type=recent';
+	}
+	if (max_id) {
+		url = url + '&max_id=' + max_id; 
+	}
 	return twitter_get(url).then(function(data) {
-		return parse_and_filter_tweets(data['statuses']);
+		if (max_id) {
+			var tweets = data['statuses'].slice(1);
+		} else {
+			var tweets = data['statuses'];
+		}
+		var parse_promise = parse_and_filter_tweets(tweets);
+		if (count > 100) {
+			var more_promise = get_tweets(hashtag, count-100, max_id);
+			return RSVP.all([parse_promise, more_promise]).then(function(responses) {
+				return responses[0].concat(responses[1]);
+			});
+		} else {
+			return parse_promise;
+		}
 	});	
 }
 
-var hashtag = 'islamophobia';
-get_popular_tweets(hashtag).then(function(tweets) {
-	console.log(tweets);
-});
+function intersection(a1,a2) {
+	return a1.filter(function(e1) {
+    return a2.indexOf(e1) != -1;
+	});	
+}
 
-// twit.get('/statuses/retweets/525664776157163520.json', function(data) {
-//   console.log(util.inspect(data.map(function(tweet) {
-//   	return tweet.entities.urls.map(function(url) { return url.expanded_url});
-//   })));
-// });
+// var hashtag = 'islamophobia';
+// var hashtag = 'obama';
+var hashtag = 'ebola';
+get_tweets(hashtag, 300).then(function(tweets) {
+	console.log(tweets);
+	console.log(tweets.length);
+	// console.log(tweets[0].retweeters.length);
+	// console.log(intersection(tweets[0].retweeters,tweets[1].retweeters));
+}).catch(function(error) {
+  console.log(error);
+});
